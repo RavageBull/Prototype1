407df5da9ce81d18b5dadcec22587b2b920636b5
aaaa <1012334@student.sae.edu.au>
2019-10-10 09:17:34 +0800

    Menu done and dusted

---
+++ Prototype1/Assets/Scripts/ResolutionManager.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ResolutionManager : MonoBehaviour
+{
+
+    public int width;
+    public int height;
+
+    public void setWidth(int newWidth)
+    {
+        width = newWidth;
+    }
+
+    public void setHeight(int newHeight)
+    {
+        width = newHeight;
+    }
+
+    public void setRes()
+    {
+        Screen.SetResolution(width, height, false);
+    }
+}
---
+++ Prototype1/Assets/Scripts/ScrollTexture.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScrollTexture : MonoBehaviour
+{
+    public float scrollAmount;
+    private Renderer render;
+    // Start is called before the first frame update
+    void Awake()
+    {
+        render = GetComponent<Renderer>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        render.material.mainTextureOffset = new Vector2(render.material.mainTextureOffset.x + (scrollAmount * Time.smoothDeltaTime), render.material.mainTextureOffset.y);
+    }
+}
